6d794554b79783aab95cdb445b6e30a973f403d0
nnk <35590626+nnnk7777@users.noreply.github.com>
2019-05-10 14:43:20 +0900

    FirstCommit

---
+++ Otofuda-iOS/src/Main/Top/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Otofuda-iOS
+//
+//  Created by nonaka on 2019/05/10.
+//  Copyright © 2019 nkmr-lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
