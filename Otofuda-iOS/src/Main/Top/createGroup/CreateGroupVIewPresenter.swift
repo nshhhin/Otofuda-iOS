8509594d0764957f01ec7ce92e6c1fe53c517fd6
nnk <electro0701@gmail.com>
2019-06-15 10:40:17 +0900

    install firebase

---
+++ Otofuda-iOS/src/Main/Top/createGroup/CreateGroupVIewPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  CreateGroupVIewPresenter.swift
+//  Otofuda-iOS
+//
+//  Created by nonaka on 2019/05/11.
+//  Copyright © 2019 nkmr-lab. All rights reserved.
+//
+
+import Foundation
--- Otofuda-iOS/src/Main/Top/top/TopViewController.swift
+++ Otofuda-iOS/src/Main/Top/top/TopViewController.swift
@@ -15,15 +15,20 @@ class OtofudaTopViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    // グループを作成するボタン
     @IBAction func tapCreateBtn(_ sender: Any) {
-        // ルームを作るボタンをタップしたときの処理
+        // QRコードを生成して表示する画面に遷移
         let next =  storyboard!.instantiateViewController(withIdentifier: "CreateGroupView")
         next.modalTransitionStyle = .crossDissolve
         self.present(next,animated: true, completion: nil)
     }
     
+    // グループに参加するボタン
     @IBAction func tapSearchBtn(_ sender: Any) {
-        // カメラ起動（QRコード読み込み）
+        // カメラを起動してQRコードを読み取る画面に遷移
+        let next =  storyboard!.instantiateViewController(withIdentifier: "SearchGroupView")
+        next.modalTransitionStyle = .crossDissolve
+        self.present(next,animated: true, completion: nil)
     }
 }
 
