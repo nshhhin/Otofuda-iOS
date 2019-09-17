c1efb4de921cc845ff8f345af093f58b0d35bedd
nshhhin <shinn070110@gmail.com>
2019-09-17 15:15:05 +0900

    :heavy_plus_sign: iTunesPickerVCを追加

---
+++ Otofuda-iOS/src/Main/Menu/iTunesPicker/iTunesPicker+UISearchBarDelegate.swift
@@ -0,0 +1,44 @@
+
+import UIKit
+import Alamofire
+import AlamofireImage
+import SwiftyJSON
+import PromiseKit
+
+extension iTunesPickerVC: UISearchBarDelegate {
+    
+    // 検索ボタン押下時の呼び出しメソッド
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.endEditing(true)
+        searchMusic(keyword: searchBar.text!,
+                    attribute: selectedAttribute)
+    }
+    
+    @IBAction func segmentChanged(_ sender: Any) {
+        
+        if segumentView.selectedSegmentIndex == 0 {
+            selectedAttribute = "artistTerm"
+        } else {
+            selectedAttribute = "songTerm"
+        }
+        
+        searchMusic(keyword: searchBar.text!,
+                    attribute: selectedAttribute)
+    }
+    
+    func searchMusic(keyword: String, attribute: String){
+        firstly {
+            iTunesAPIModel.shared.request(keyword: searchBar.text!,
+                                          attribute: attribute)
+            }.then { data -> Promise<Results> in
+                return iTunesAPIModel.shared.mapping(jsonStr: data)
+            }.done { results in
+                print("done")
+                print(results)
+                self.results = results
+                self.collectionView.reloadData()
+            }.catch { error in
+                print(error)
+        }
+    }   
+}
