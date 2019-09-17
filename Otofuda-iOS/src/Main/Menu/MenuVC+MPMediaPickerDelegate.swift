31c59515a9f89b4804c045e2a48d0f4c5e3fbb62
nshhhin <shinn070110@gmail.com>
2019-09-17 13:45:34 +0900

    :art: リファクタリング

--- Otofuda-iOS/src/Main/Menu/MenuVC+MPMediaPickerDelegate.swift
+++ Otofuda-iOS/src/Main/Menu/MenuVC+MPMediaPickerDelegate.swift
@@ -7,8 +7,7 @@ extension MenuVC: MPMediaPickerControllerDelegate {
     @IBAction func tappedPickBtn(_ sender: Any) {
         let picker = MPMediaPickerController()
         picker.delegate = self
-        // 複数選択にする。（falseにすると、単数選択になる）
-        picker.allowsPickingMultipleItems = true
+        picker.allowsPickingMultipleItems = true // 複数選択可
         present(picker, animated: true, completion: nil)
     }
     
