3eea3d6264beade598998936b562edf0b578149f
nshhhin <shinn070110@gmail.com>
2019-09-21 21:48:02 +0900

    :heavy_plus_sign: とりあえずテストファイルを追加した

--- Otofuda-iOS/src/Main/Menu/MenuVC.swift
+++ Otofuda-iOS/src/Main/Menu/MenuVC.swift
@@ -79,11 +79,6 @@ final class MenuVC: UIViewController, Menurotocol {
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "next" {
-            //            var arrayDict: [Dictionary<String,Any>] = []
-            //            for music in selectedMusics {
-            //                arrayDict.append( music.dict() )
-            //            }
-            //            firebaseManager.post(path: room.url() + "selected", value: arrayDict)
 
             room.status = .play
 
